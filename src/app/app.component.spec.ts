c632f2b1164eef35ed7dc43c6aa975b0aba513c5
= <galvanize9@fvfx1cf5j1wl.ci.org>
2019-09-30 11:23:27 -0600

    first test success

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngshop'`, () => {
+  it(`should have as title 'The Bulldog Shop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngshop');
+    expect(app.title).toEqual('The Bulldog Shop');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngshop!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to The Bulldog Shop!');
   });
 });
