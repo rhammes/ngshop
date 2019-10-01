a28b3d20d31d0c38e24540367c1430458e12ec54
= <galvanize9@fvfx1cf5j1wl.ci.org>
2019-10-01 11:55:29 -0600

    Added favorites block to app

--- src/app/favorites/favorites.component.spec.ts
+++ src/app/favorites/favorites.component.spec.ts
@@ -1,10 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FavoritesComponent } from './favorites.component';
+import { Dog } from '../dog';
 
 describe('FavoritesComponent', () => {
   let component: FavoritesComponent;
   let fixture: ComponentFixture<FavoritesComponent>;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,9 +19,28 @@ describe('FavoritesComponent', () => {
     fixture = TestBed.createComponent(FavoritesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    
+    compiled = fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a list of dog names', () => {
+    const dog1: Dog = new Dog();
+    const dog2: Dog = new Dog();
+    dog1.id = "dog1";
+    dog1.name = "Name Dog1";
+    dog2.id = "dog2";
+    dog2.name = "Name Dog2";
+    component.favorite.toggleDog(dog1);
+    component.favorite.toggleDog(dog2);
+    fixture.detectChanges();
+
+    const favorites = compiled.querySelector('.favorites-component')
+    expect(favorites.textContent).toContain(dog1.name);
+    expect(favorites.textContent).toContain(dog2.name);
+
+  })
 });
--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Favorite } from '../favorite';
 
 @Component({
   selector: 'app-favorites',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoritesComponent implements OnInit {
 
-  constructor() { }
+  favorite: Favorite;
+  
+  constructor() {
+    this.favorite = new Favorite();
+  }
 
   ngOnInit() {
   }
