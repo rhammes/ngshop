6f55b49b6121fd037f96f2fd17b12c4c6d8f9b2f
= <galvanize9@fvfx1cf5j1wl.ci.org>
2019-10-01 12:01:30 -0600

    Add 2 static favorites

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Favorite } from '../favorite';
+import { Dog } from '../dog';
 
 @Component({
   selector: 'app-favorites',
@@ -12,6 +13,16 @@ export class FavoritesComponent implements OnInit {
   
   constructor() {
     this.favorite = new Favorite();
+
+    const dog1: Dog = new Dog();
+    const dog2: Dog = new Dog();
+    dog1.id = "dog1";
+    dog1.name = "Name Dog1";
+    dog2.id = "dog2";
+    dog2.name = "Name Dog2";
+    this.favorite.toggleDog(dog1);
+    this.favorite.toggleDog(dog2);
+
   }
 
   ngOnInit() {
